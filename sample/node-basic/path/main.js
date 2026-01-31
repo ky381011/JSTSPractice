84e5b7332da154a98adc80db1afa4ff3e3924a72
ky381011 <ky381011git@gmail.com>
2026-01-31 22:48:55 +0900

    更新: __dirnameの表示とdist/sample.txtの絶対パスを解決するコードを追加

--- sample/node-basic/path/main.js
+++ sample/node-basic/path/main.js
@@ -2,3 +2,7 @@ const fs = require('fs'); // fsモジュールを読み込む
 const path = require('path'); // pathモジュールを読み込む
 
 console.log('__dirname:', __dirname); // このファイル自体が存在する現在のディレクトリの絶対パスを表示
+
+console.log("+       => " + __dirname + "../dist/sample.txt")
+const distPath = path.resolve(__dirname, "../dist/sample.txt")
+console.log('resolve =>', distPath); // ../dist/sample.txtの絶対パスを表示
