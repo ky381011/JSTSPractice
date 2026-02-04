93b9bc1a411d2bdf700f102cc2b06c5e2dd1ecd9
ky381011 <ky381011git@gmail.com>
2026-02-04 23:12:24 +0900

    新規作成: エスケープシーケンスとリテラルの例を含むコードをescape.tsとliteral.tsに追加 新規作成: 変数の型注釈と代入の例を含むコードをvariable.tsに追加

---
+++ sample/ts/variable-type/variable.ts
@@ -0,0 +1,8 @@
+let color: string = 'Red'; // stringが型注釈されている
+console.log(color);
+
+color = 'Green'; // 別の文字列を代入可能
+console.log(color);
+
+// let color: string = 'Blue'; // 再宣言はエラーになる
+// color = 123; // 異なる型の値を代入しようとするとエラーになる
