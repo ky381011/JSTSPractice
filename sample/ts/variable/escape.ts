a6df7fcdebf50cfe823363dc0993dcd9ec59f659
ky381011 <ky381011git@gmail.com>
2026-02-04 23:07:16 +0900

    新規作成: エスケープシーケンスの例を含むコードをescape.tsに追加

---
+++ sample/ts/variable/escape.ts
@@ -0,0 +1,8 @@
+console.log('Hello! I\'m TypeScript.'); // シングルクォートのエスケープ
+console.log('\\ \t AAA \n BBB'); // バックスラッシュ、タブ、改行のエスケープ
+
+console.log("Hello! I'm TypeScript."); // ダブルクォートを利用してシングルクォートをエスケープ不要に
+console.log('Hello! "TypeScript".'); // シングルクォートを利用してシングルクォートをエスケープ
+
+console.log('Hello');
+console.log("Hello");
