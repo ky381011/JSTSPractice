169c60874d0d1854deb9ff8aaa2ce145095bc4fe
ky381011 <ky381011git@gmail.com>
2026-02-05 23:58:40 +0900

    新規作成: type.tsファイルを追加し、文字列、数値、ブール値の型の例を実装

---
+++ sample/ts/variable-type/type.ts
@@ -0,0 +1,15 @@
+let season: string = 'Summer';
+console.log(season);
+console.log(typeof season);
+
+let num: number = 123;
+console.log(num);
+console.log(typeof num);
+
+let dec = 456.789; // 型注釈を省略すると、型推論によりnumber型と推論される
+console.log(dec);
+console.log(typeof dec);
+
+let isActive: boolean = true;
+console.log(isActive);
+console.log(typeof isActive);
