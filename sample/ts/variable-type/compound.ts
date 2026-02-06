243ec8ae8f9248083a91c0854aafe8d96f97c57d
ky381011 <ky381011git@gmail.com>
2026-02-06 23:55:17 +0900

    新規作成: compound.tsファイルを追加し、複合演算子を用いた算術演算の例を実装

---
+++ sample/ts/variable-type/compound.ts
@@ -0,0 +1,24 @@
+// 通常の加算
+let num0 = 20;
+num0 = num0 + 5;
+console.log(num0);
+
+// 複合演算子による加算
+let num1 = 20;
+num1 += 5;
+console.log(num1);
+
+// 複合演算子による減算
+let num2 = 20;
+num2 -= 5;
+console.log(num2);
+
+// 複合演算子による乗算
+let num3 = 20;
+num3 *= 5;
+console.log(num3);
+
+// 複合演算子による除算
+let num4 = 20;
+num4 /= 5;
+console.log(num4);
