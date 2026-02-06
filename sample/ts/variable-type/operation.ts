70ae0c2c0d0d061a46a11cd99869bb69d22b82c6
ky381011 <ky381011git@gmail.com>
2026-02-06 23:51:23 +0900

    新規作成: operation.tsファイルを追加し、基本的な算術演算の例を実装

---
+++ sample/ts/variable-type/operation.ts
@@ -0,0 +1,28 @@
+// Addition
+const ans = 1 + 2;
+console.log(ans);
+
+// Subtraction
+const i = 5;
+const j = 3;
+console.log(i - j);
+
+// Multiplication
+const x = 4;
+const y = 2;
+console.log(x * y);
+
+// Division
+const dividend = 9;
+const divisor = 2;
+console.log(dividend / divisor);
+
+// Modulus
+const dividendMod = 10;
+const divisorMod = 3;
+console.log(dividendMod % divisorMod);
+
+// Exponentiation
+const base = 2;
+const exponent = 3;
+console.log(base ** exponent);
