9e2fb499e344c27b2a95924763754071fc663e0a
ky381011 <ky381011git@gmail.com>
2026-02-07 22:47:00 +0900

    新規作成: increment-decrement.tsファイルを追加し、インクリメントとデクリメントの演算子の例を実装

---
+++ sample/ts/variable-type/increment-decrement.ts
@@ -0,0 +1,21 @@
+let num = 10;
+// 後置演算子
+// インクリメント
+num++;
+console.log(num); // Output: 11
+
+// デクリメント
+num--;
+console.log(num); // Output: 10
+
+// 前置演算子
+// インクリメント
+++num;
+console.log(num); // Output: 11
+
+// デクリメント
+--num;
+console.log(num); // Output: 10
+
+// 前置演算子はインクリメントをしてから値を返す
+// 後置演算子は値を返してからインクリメントをする
