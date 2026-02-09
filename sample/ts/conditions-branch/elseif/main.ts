adff3a8365831c26a0ad89e5904cbfc4fae60db5
ky381011 <ky381011git@gmail.com>
2026-02-09 23:31:41 +0900

    新規作成: main.tsファイルを追加し、条件分岐による成績判定の例を実装

---
+++ sample/ts/conditions-branch/elseif/main.ts
@@ -0,0 +1,11 @@
+const point: number = 100;
+
+if (point >= 90) {
+    console.log("A");
+} else if (point >= 80) {
+    console.log("B");
+} else if (point >= 60) {
+    console.log("C");
+} else {
+    console.log("D");
+}
