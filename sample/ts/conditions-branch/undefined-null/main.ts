7588c4ccd72f249aeeeb229330bb5ff456313408
ky381011 <ky381011git@gmail.com>
2026-02-11 00:02:16 +0900

    新規作成: main.tsファイルを追加し、undefinedとnullの例を実装

---
+++ sample/ts/conditions-branch/undefined-null/main.ts
@@ -0,0 +1,14 @@
+let x;
+console.log(x); // undefined
+
+let item = null;
+console.log(item); // null
+console.log(typeof item); // object
+
+if (x === undefined) {
+    console.log("x is undefined");
+}
+
+if (item === null) {
+    console.log("item is null");
+}
