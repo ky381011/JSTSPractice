e774cd99e005d1fc6d4fbeba061ed32e8dba17d9
ky381011 <ky381011git@gmail.com>
2026-02-13 23:56:25 +0900

    新規作成: タプルの例をmain.tsファイルに追加

---
+++ sample/ts/array-object-tuple/tuple/main.ts
@@ -0,0 +1,8 @@
+export {};
+const user: [string, number, string] = ['Liam', 20, 'Tokyo'];
+console.log(user[0]); // "Liam"
+console.log(user[1]); // 20
+console.log(user[2]); // "Tokyo"
+
+user[1] = 21;
+console.log(user[1]); // 21
