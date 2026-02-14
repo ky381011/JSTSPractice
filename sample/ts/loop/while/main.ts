8cecee96a12198808c2c5a00e0cf85311b1841b6
ky381011 <ky381011git@gmail.com>
2026-02-14 22:21:55 +0900

    新規作成: whileループの例をmain.tsファイルに追加

---
+++ sample/ts/loop/while/main.ts
@@ -0,0 +1,14 @@
+let i = 1;
+while(i <= 5) {
+    console.log(i);
+    i++;
+}
+
+let j = 0;
+while(j <= 10) {
+    console.log(j);
+    if(j === 5) {
+        break; // ループを終了する
+    }
+    j++;
+}
