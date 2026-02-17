e60532ad7c61851f1f159c91993e001dfaad9297
ky381011 <ky381011git@gmail.com>
2026-02-17 22:26:20 +0900

    新規作成: sample.js、sample.ts、main.tsファイルを追加し、getSampleName関数をエクスポートする機能を実装

---
+++ sample/ts/module/function/main.ts
@@ -0,0 +1,4 @@
+import { getSampleName } from "./sample.js";
+
+console.log(`Hello, ${getSampleName()}!`);
+// exportされた関数をインポートして使用することができる。
---
+++ sample/ts/module/function/sample.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getSampleName = void 0;
+var getSampleName = function () {
+    return 'SampleName';
+};
+exports.getSampleName = getSampleName;
