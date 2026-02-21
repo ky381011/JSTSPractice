f8929cda04356a1ef79a842b8d035f69025dbff8
ky381011 <ky381011git@gmail.com>
2026-02-21 23:28:21 +0900

    index.htmlに入力フィールドにIDを追加し、index.mjsにTODO追加機能を実装

--- sample/react/regacy/index.mjs
+++ sample/react/regacy/index.mjs
@@ -1,6 +1,23 @@
 const onClickAdd = () => {
-  alert("TODOを追加しました");
+  // テキストボックスの値を取得する
+  const inputText = document.getElementById("add-text").value;
+  // tテキストボックスを初期化する
+  document.getElementById("add-text").value = "";
 
+  // liタグの生成
+  const li = document.createElement("li");
+  console.log(li);
+
+  // divタグの生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  console.log(div);
+
+  // pタグの生成
+  const p = document.createElement("p");
+  p.className = "todo-item";
+  p.innerText = inputText;
+  console.log(p);
 }
 
 document.getElementById("add-button").addEventListener("click", onClickAdd);
