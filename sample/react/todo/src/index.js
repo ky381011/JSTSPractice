4042dd715bc430f4ee2dbabbb1ef81a111bff383
ky381011 <ky381011git@gmail.com>
2026-02-22 23:16:56 +0900

    不要なコメントを削除し、Appコンポーネントのコードを整理した

--- sample/react/todo/src/index.js
+++ sample/react/todo/src/index.js
@@ -5,15 +5,36 @@ const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 // コンポーネントの変数は大文字で始める
+// const App = () => {
+//   return (
+//     // returnする部分は必ず1つのタグで囲む必要がある
+//     // <div>
+//     //   <h1>こんにちは</h1>
+//     //   <p>お元気ですか？</p>
+//     // </div>
+
+//     // React.Fragmentを使うと、タグで囲む必要がなくなる
+//     // <React.Fragment>
+//     //   <h1>こんにちは</h1>
+//     //   <p>お元気ですか？</p>
+//     // </React.Fragment>
+
+//     // Fragmentは省略して書くこともできる
+//     // <>
+//     //   <h1>こんにちは</h1>
+//     //   <p>お元気ですか？</p>
+//     // </>
+//   );
+// };
+
 const App = () => {
   return (
-    // returnする部分は必ず1つのタグで囲む必要がある
     <div>
       <h1>こんにちは</h1>
       <p>お元気ですか？</p>
     </div>
   );
-};
+}
 
 root.render(
   <StrictMode>
