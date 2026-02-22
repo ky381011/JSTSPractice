31e9d60725be0d54dd93e001052dd86cd775f94b
ky381011 <ky381011git@gmail.com>
2026-02-22 23:13:54 +0900

    Appコンポーネントを作成し、root.render内で直接の要素をAppコンポーネントに置き換えた

--- sample/react/todo/src/index.js
+++ sample/react/todo/src/index.js
@@ -4,8 +4,19 @@ import { createRoot } from 'react-dom/client';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
+// コンポーネントの変数は大文字で始める
+const App = () => {
+  return (
+    // returnする部分は必ず1つのタグで囲む必要がある
+    <div>
+      <h1>こんにちは</h1>
+      <p>お元気ですか？</p>
+    </div>
+  );
+};
+
 root.render(
   <StrictMode>
-    <h1>こんにちは</h1>
+    <App />
   </StrictMode>
 );
