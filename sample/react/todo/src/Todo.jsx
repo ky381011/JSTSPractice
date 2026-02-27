bad052d1d75c134d07a4b97472ad1a25d7d89121
ky381011 <ky381011git@gmail.com>
2026-02-27 23:36:31 +0900

    Todoコンポーネントのスタイルを追加し、レイアウトを改善

--- sample/react/todo/src/Todo.jsx
+++ sample/react/todo/src/Todo.jsx
@@ -3,35 +3,43 @@ import "./style.css";
 export function Todo() {
   return (
     <>
-      <div>
+      <div className="input-area">
         <input placeholder="TODOを入力" />
         <button>追加</button>
       </div>
-      <div>
-        <p>未完了のTODO</p>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
         <ul>
           <li>
-            <span>TODO1</span>
-            <button>完了</button>
-            <button>削除</button>
+            <div className="list-row">
+              <p className="todo-item">TODO1</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
           </li>
           <li>
-            <span>TODO2</span>
-            <button>完了</button>
-            <button>削除</button>
+            <div className="list-row">
+              <p className="todo-item">TODO2</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
           </li>
         </ul>
       </div>
-      <div>
-        <p>完了のTODO</p>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
         <ul>
           <li>
-            <span>TODO3</span>
-            <button>戻す</button>
+            <div className="list-row">
+              <p className="todo-item">TODO3</p>
+              <button>戻す</button>
+            </div>
           </li>
           <li>
-            <span>TODO4</span>
-            <button>戻す</button>
+            <div className="list-row">
+              <p className="todo-item">TODO4</p>
+              <button>戻す</button>
+            </div>
           </li>
         </ul>
       </div>
