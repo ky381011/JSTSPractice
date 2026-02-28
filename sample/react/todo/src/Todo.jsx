7aa25439438d5048a15d498f5ceb23fa6e02cdbe
ky381011 <ky381011git@gmail.com>
2026-02-28 23:58:52 +0900

    IncompleteTodoコンポーネントを追加し、未完了TODOの表示を整理

--- sample/react/todo/src/Todo.jsx
+++ sample/react/todo/src/Todo.jsx
@@ -1,4 +1,5 @@
 import { InputTodo } from "./components/InputTodo";
+import { IncompleteTodo } from "./components/IncompleteTodo";
 import "./style.css";
 
 import { useState } from "react";
@@ -59,22 +60,11 @@ export function Todo() {
         onChange={onChangeTodoText}
         onClick={onClickAdd}      
       />
-      <div className="incomplete-area">
-        <p className="title">未完了のTODO</p>
-        <ul>
-          {incompleteTodos.map((todo, index) => { // indexは順番が変わる場合があるため、keyには使用しない
-            return (
-              <li key={todo}>
-                <div className="list-row">
-                  <p className="todo-item">{todo}</p>
-                  <button onClick={() => onClickComplete(index)}>完了</button>
-                  <button onClick={() => onClickDelete(index)}>削除</button> {/* onClickDeleteに引数を渡すために、無名関数でラップする */ }
-                </div>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+      <IncompleteTodo
+        todos={incompleteTodos}
+        onClickComplete={onClickComplete}
+        onClickDelete={onClickDelete}
+      />
       <div className="complete-area">
         <p className="title">完了のTODO</p>
         <ul>
