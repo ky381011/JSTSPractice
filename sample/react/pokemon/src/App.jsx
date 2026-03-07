0a83178aa0597257ce1e866b0b245f18585f1a7a
ky381011 <ky381011git@gmail.com>
2026-03-07 16:58:15 +0900

    feat: ポケモンデータの読み込み中に表示するローディングメッセージを追加

--- sample/react/pokemon/src/App.jsx
+++ sample/react/pokemon/src/App.jsx
@@ -5,19 +5,24 @@ import { getAllPokemon } from "./utils/pokemon";
 
 export function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true);
+
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       // すべてのポケモンのデータを取得するためのロジック
       let res = await getAllPokemon(initialURL);
       console.log(res);
+      setLoading(false);
     }
     fetchPokemonData();
   }, []);
 
   return (
     <>
-      <div className="App"></div>
+      <div className="App">
+        {loading ? <h1>Loading Now...</h1> : <h1>Loading Complete!</h1>}
+      </div>
     </>
   );
 }
