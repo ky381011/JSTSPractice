7be098ba6d830c671a1f8032ccc8b0e4d81a5180
ky381011 <ky381011git@gmail.com>
2026-03-15 02:11:28 +0900

    feat: Sidebarコンポーネントにノート追加機能を実装し、スタイルを調整

--- sample/react/note/src/App.jsx
+++ sample/react/note/src/App.jsx
@@ -1,13 +1,28 @@
 import './App.css'
 
+import { useState } from 'react'
+
 import Sidebar from './components/Sidebar'
 import Main from './components/Main'
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+
+  const onAddNote = () => {
+    console.log('ノートを追加');
+    const newNote = {
+      id: 1,
+      title: '新しいノート',
+      content: 'ノートの内容',
+      modDate: Date.now(),
+    };
+    setNotes([...notes, newNote]);
+  }
   return (
     <>
       <div className='App'>
-        <Sidebar />
+        <Sidebar onAddNote={onAddNote} />
         <Main />
       </div>
     </>
